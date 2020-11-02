c4eacb69f92e24a1f8a0323f29a9ea0b6a7431fc
Scorcher <706439+Scorcher@users.noreply.github.com>
2020-11-03 00:32:40 +0300

    substitute dot to underline in path

--- cgp-implemenation.go
+++ cgp-implemenation.go
@@ -147,6 +147,7 @@ func handleCommandGetFile(w http.ResponseWriter, req *http.Request, path string)
 	var emailLocalPart string
 
 	emailPrefix = fmt.Sprintf("%s.sub/%s.sub", string(email[0]), string(email[1]))
+	emailPrefix = strings.Replace(emailPrefix, "..", "_.", 2)
 	emailLocalPart = strings.SplitN(email, "@", 2)[0]
 
 	var fullPath string
